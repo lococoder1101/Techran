757161a1233ef38e81f9743ba9070f20a76b1f8e
locoCoder <lococoder1101@gmail.com>
2019-09-08 12:02:56 +0430

    Creating Routing module and designing header

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {NewsComponent} from './news/news.component';
+import {AuthorComponent} from "./author/author.component";
+import {ContactComponent} from "./contact/contact.component";
+
+const routes: Routes = [
+  { path: '', component: NewsComponent },
+  { path: 'author', component: AuthorComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'authors', component: AuthorComponent },
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { AuthorComponent } from './author/author.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthorComponent,
+    FooterComponent,
+    NewsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
