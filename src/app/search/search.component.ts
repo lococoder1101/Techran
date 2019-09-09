e1ad28482c9482bb3ab78a6417f83bcd49825e4b
locoCoder <lococoder1101@gmail.com>
2019-09-09 19:46:19 +0430

    improving search box and new style

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,7 @@ import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core
 import {Observable} from 'rxjs';
 import {DataService} from 'src/app/data.service';
 import {debounceTime, map} from 'rxjs/operators';
+import { Router } from "@angular/router";
 
 
 @Component({
@@ -17,7 +18,7 @@ export class SearchComponent implements OnInit {
   @ViewChild('div') div: ElementRef;
   private authors: any;
 
-  constructor(private  dataService: DataService, private renderer: Renderer2) {}
+  constructor(private  dataService: DataService, private renderer: Renderer2,private router: Router) {}
 
   ngOnInit() {
     this.dataService.getAuthor().subscribe(data => {
@@ -34,22 +35,22 @@ search = (text$: Observable<string>) =>
       : this.authors.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
   )
 formatter = (x: { name: string }) => x.name;
+  highlightAuthor: any;
 
-addElement(name: string, image: string) {
-  const p: HTMLParagraphElement = this.renderer.createElement('li');
-  p.className = 'list-inline-item';
-  p.innerHTML =
-    // tslint:disable-next-line:max-line-length
-    '<button   class="btn" style="font-size: 12px;margin: 5px"><img style="height: 30px;width: 30px;margin-right: 5px" src=' + image + '>' +
-    name + '</button>';
-
-
-  this.renderer.appendChild(this.div.nativeElement, p);
-}
 
   getProfileImages(userId:any){
     return this.dataService.getProfileImage(userId);
   }
 
+  goToAuthorPage(id: any) {
+    var myurl = '/author/'+id;
+    this.router.navigateByUrl(myurl).then(e => {
+      if (e) {
+        console.log("Navigation is successful!");
+      } else {
+        console.log("Navigation has failed!");
+      }
+    });
+  }
 }
 
